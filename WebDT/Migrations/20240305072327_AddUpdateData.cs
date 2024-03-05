6ebcc58d344a697b104425d65d8173391c002be6
DoManhHung <domanhhung6404@gmail.com>
2024-03-05 16:13:54 +0700

    UpdateData

---
+++ WebDT/Migrations/20240305072327_AddUpdateData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDT.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdateData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TenSanPham",
+                table: "SanPham",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenSanPham",
+                table: "SanPham");
+        }
+    }
+}
