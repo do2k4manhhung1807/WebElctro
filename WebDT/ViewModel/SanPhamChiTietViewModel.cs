2843ea0b6ab990fed4f7ab3b8e071c0f478c83af
DoManhHung <domanhhung6404@gmail.com>
2024-03-12 18:44:32 +0700

    UI Product

--- WebDT/ViewModel/SanPhamChiTietViewModel.cs
+++ WebDT/ViewModel/SanPhamChiTietViewModel.cs
@@ -3,6 +3,10 @@ namespace WebDT.ViewModel
 {
     public class SanPhamChiTietViewModel
     {
+        public List<Iphone> Iphone { get; set; }  
+        public List<Ipad> Ipad { get; set; }
+        public List<IMac> IMac { get; set; }
+        public List<Laptop> Laptop { get; set; }
         public List<SanPham> SanPhamList { get; set; }
         public List<HinhAnh> HinhAnhList { get; set; }
     }
