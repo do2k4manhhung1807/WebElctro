313752a80965013e76c6244006db6114d4f0b9df
harrynguyen0710 <nnhoang0710@gmail.com>
2024-03-19 22:54:49 +0700

    Add shopping cart and UI

--- WebDT/ViewModel/CartItemViewModel.cs
+++ WebDT/ViewModel/CartItemViewModel.cs
@@ -6,11 +6,7 @@ namespace WebDT.ViewModel
     {
         public List<CartItemModel> CartItems { get; set; }
         public DonHang DonHang { get; set; } 
-/*        public ChiTietDonHangSanPham ChiTietDonHang { get; set; }
-*/        
         public decimal GrandTotal { get; set; } //Tinh tong gia
-        /*        public int TongSoLuongHienThi => CartItems.Sum(x => x.Soluong);
-        */
         public int TongSoLuongHienThi { get; set; } 
     }
 }
